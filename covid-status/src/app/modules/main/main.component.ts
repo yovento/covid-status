c61134be4a3e393f13aef3ddea1300de7765f900
John Restrepo <jovento@gmail.com>
2020-07-20 20:46:17 -0500

    Added auto scroll capabilities and some style fixes

--- covid-status/src/app/modules/main/main.component.ts
+++ covid-status/src/app/modules/main/main.component.ts
@@ -17,5 +17,13 @@ export class MainComponent implements OnInit {
   selectedCountryChanged(country) {
     this.selectedCountry = country;
     this.countrydetailsComponent.selectedCountryChanged(country);
+    let scrollToTop = window.setInterval(() => {
+      let pos = window.pageYOffset;
+      if (pos > 0) {
+        window.scrollTo(0, pos - 50); // how far to scroll on each step
+      } else {
+        window.clearInterval(scrollToTop);
+      }
+    }, 16);
   }
 }
